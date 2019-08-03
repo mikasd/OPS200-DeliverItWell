a711a41465f05dafbc70216ddd2c02fb18608cee
mikeingley <michael.ingley@gmail.com>
2019-08-03 16:45:05 -0700

    update

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,5 +37,15 @@ describe('server/app.js', function() {
       done();
     });
   });
-  
+
+  it('page says tests', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('tests');
+        done();
+      });
+    });
+
 })
